62487ebc256db0a4749e9f62fcd131df0fb00c20
Parker P <exxcaelum@gmail.com>
2017-02-13 02:32:42 -0600

    change test site to one of my own

--- test/crawler_test.rb
+++ test/crawler_test.rb
@@ -3,11 +3,11 @@ require_relative 'test_helper'
 class CrawlerTest < Minitest::Test
 
   def setup
-    @crawler = Crawler.new("http://wiprodigital.com/")
+    @crawler = Crawler.new("http://copper-falls-market.herokuapp.com")
   end
 
   def test_crawler_can_get_home_page_links
     @crawler.get_links
-    assert_equal 334, @crawler.links.length
+    assert_equal 176, @crawler.links.length
   end
 end
