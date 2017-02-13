bf3519b53b112248ba63e73b599c568a7c4d70a9
Parker P <exxcaelum@gmail.com>
2017-02-12 22:38:05 -0600

    write tests for crawler, write pseudo code for crawler

--- lib/crawler.rb
+++ lib/crawler.rb
@@ -1,3 +1,33 @@
+require 'open-uri'
+require 'nokogiri'
+
 class Crawler
+  attr_reader :url,
+              :page,
+              :links
+
+  def initialize(url)
+    @links = []
+    @visited = []
+    @url = url
+    @page = Nokogiri::HTML(open(url))
+  end
+
+  def fetch
+    # go to home page
+    # add homepage to visited
+    # add links on home page to link array
+    # visit next page if not already visited
+    # add page to visited
+    # add links to links array
+    # repeat until counter is same number as array
+  end
+
+
+  def get_links
+    # parse through all anchor tags
+    # find the href of the anchor tag and put it in the link array
+    # remove duplicates
+  end
 
 end
--- test/crawler_test.rb
+++ test/crawler_test.rb
@@ -2,4 +2,20 @@ require_relative 'test_helper'
 
 class CrawlerTest < Minitest::Test
 
+  def setup
+    @crawler = Crawler.new("http://wiprodigital.com/")
+  end
+
+  def test_crawler_has_website
+    assert_equal "http://wiprodigital.com/", @crawler.url
+  end
+
+  def test_crawler_has_xml
+    assert_equal Nokogiri::XML::NodeSet, @crawler.page.css('body').class
+  end
+
+  def test_crawler_can_get_home_page_links
+    @crawler.get_links
+    assert_equal 55, @crawler.links.length
+  end
 end
