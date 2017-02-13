02eb717d7025ed8f3fc1c9b235459671ad3c5d37
Parker P <exxcaelum@gmail.com>
2017-02-13 16:32:51 -0600

    added clarity and structure to the crawl.rb file

--- crawl.rb
+++ crawl.rb
@@ -1,13 +1,26 @@
 require_relative 'lib/crawler'
 
-url = "race-condition.org"
+# This url determines which website you will be scraping
+url = "http://wiprodigital.coom"
 
+# If you encounter a bad URI error please check the spelling of the website
+
+
+# DO NOT TOUCH CODE BELOW THIS LINE --------------------------------------------
 crawler = Crawler.new(url)
 crawler.get_links
+
 title = crawler.domain.gsub("http://www.", "")
+
 found_domains = crawler.links.sort
-sub_domains = found_domains.find_all {|target_domain| target_domain.include?(title)}
-external_domains = found_domains.find_all {|target_domain| !target_domain.include?(title)}
+
+sub_domains = found_domains.find_all do |target_domain|
+  target_domain.include?(title)
+end
+
+external_domains = found_domains.find_all do |target_domain|
+   !target_domain.include?(title)
+ end
 
 File.open("./scraped/#{title}.md", "w+") do |file|
   file.puts(found_domains)
