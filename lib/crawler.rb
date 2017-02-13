281f5d48e09903ae38dcaf9ca01de97f614f35ea
Parker P <exxcaelum@gmail.com>
2017-02-13 01:16:01 -0600

    removed fetch method, going forward recursively

--- lib/crawler.rb
+++ lib/crawler.rb
@@ -13,17 +13,6 @@ class Crawler
     @page = Nokogiri::HTML(open(url))
   end
 
-  def fetch
-    # go to home page
-    # add homepage to visited
-    # add links on home page to link array
-    # visit next page if not already visited
-    # add page to visited
-    # add links to links array
-    # repeat until counter is same number as array
-  end
-
-
   def get_links
     @links = page.search('a').map do |link|
       link['href']
