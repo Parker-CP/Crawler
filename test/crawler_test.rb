494deb8e85109052355962f652ba077f44e44276
Parker P <exxcaelum@gmail.com>
2017-02-13 01:47:32 -0600

    remove tests that are no longer applicable

--- test/crawler_test.rb
+++ test/crawler_test.rb
@@ -6,14 +6,6 @@ class CrawlerTest < Minitest::Test
     @crawler = Crawler.new("http://wiprodigital.com/")
   end
 
-  # def test_crawler_has_website
-  #   assert_equal "http://wiprodigital.com/", @crawler.url
-  # end
-  #
-  # def test_crawler_has_xml
-  #   assert_equal Nokogiri::XML::NodeSet, @crawler.page.css('body').class
-  # end
-
   def test_crawler_can_get_home_page_links
     @crawler.get_links
     assert_equal 49, @crawler.links.length
