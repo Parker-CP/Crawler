8d1cabc324e8ddc849a25d7c504587d1e1934c28
Parker P <exxcaelum@gmail.com>
2017-02-13 01:46:54 -0600

    change test to account for bad links on home page

--- test/crawler_test.rb
+++ test/crawler_test.rb
@@ -6,16 +6,16 @@ class CrawlerTest < Minitest::Test
     @crawler = Crawler.new("http://wiprodigital.com/")
   end
 
-  def test_crawler_has_website
-    assert_equal "http://wiprodigital.com/", @crawler.url
-  end
+  # def test_crawler_has_website
+  #   assert_equal "http://wiprodigital.com/", @crawler.url
+  # end
+  #
+  # def test_crawler_has_xml
+  #   assert_equal Nokogiri::XML::NodeSet, @crawler.page.css('body').class
+  # end
 
-  def test_crawler_has_xml
-    assert_equal Nokogiri::XML::NodeSet, @crawler.page.css('body').class
-  end
-
-  def test_crawler_can_get_all_page_links
+  def test_crawler_can_get_home_page_links
     @crawler.get_links
-    assert_equal 334, @crawler.links.length
+    assert_equal 49, @crawler.links.length
   end
 end
